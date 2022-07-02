d50604ceb4658ebba8728a3fad32f4f15633200e
RCHCHISTORIA <historia.magazine.rchc@gmail.com>
2022-07-02 23:28:18 +0530

    Historia

---
+++ puzzle_answer.js
@@ -0,0 +1,260 @@
+//Across 1
+const correctWord_A1 = 'horus'
+const itemsToBeDisplayed_A1 = document.querySelectorAll('.id_A1')
+const form_A1 = document.getElementById('form2')
+
+form_A1.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A1 = document.getElementById('text_A1').value
+
+	if (inputText_A1 === '' || inputText_A1 === null) return
+	if (inputText_A1 === correctWord_A1) {
+		itemsToBeDisplayed_A1.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Across 2
+const correctWord_A2 = 'thoth'
+const itemsToBeDisplayed_A2 = document.querySelectorAll('.id_A2')
+const form_A2 = document.getElementById('form2')
+
+form_A2.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A2 = document.getElementById('text_A2').value
+
+	if (inputText_A2 === '' || inputText_A2 === null) return
+	if (inputText_A2 === correctWord_A2) {
+		itemsToBeDisplayed_A2.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Across 3
+const correctWord_A3 = 'papyrus'
+const itemsToBeDisplayed_A3 = document.querySelectorAll('.id_A3')
+const form_A3 = document.getElementById('form2')
+
+form_A3.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A3 = document.getElementById('text_A3').value
+
+	if (inputText_A3 === '' || inputText_A3 === null) return
+	if (inputText_A3 === correctWord_A3) {
+		itemsToBeDisplayed_A3.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Across 4
+const correctWord_A4 = 'ibu'
+const itemsToBeDisplayed_A4 = document.querySelectorAll('.id_A4')
+const form_A4 = document.getElementById('form2')
+
+form_A4.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A4 = document.getElementById('text_A4').value
+
+	if (inputText_A4 === '' || inputText_A4 === null) return
+	if (inputText_A4 === correctWord_A4) {
+		itemsToBeDisplayed_A4.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Across 5
+const correctWord_A5 = 'arpendonapti'
+const itemsToBeDisplayed_A5 = document.querySelectorAll('.id_A5')
+const form_A5 = document.getElementById('form2')
+
+form_A5.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A5 = document.getElementById('text_A5').value
+
+	if (inputText_A5 === '' || inputText_A5 === null) return
+	if (inputText_A5 === correctWord_A5) {
+		itemsToBeDisplayed_A5.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Across 6
+const correctWord_A6 = 'pharaohs'
+const itemsToBeDisplayed_A6 = document.querySelectorAll('.id_A6')
+const form_A6 = document.getElementById('form2')
+
+form_A6.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A6 = document.getElementById('text_A6').value
+
+	if (inputText_A6 === '' || inputText_A6 === null) return
+	if (inputText_A6 === correctWord_A6) {
+		itemsToBeDisplayed_A6.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Across 7
+const correctWord_A7 = 'senet'
+const itemsToBeDisplayed_A7 = document.querySelectorAll('.id_A7')
+const form_A7 = document.getElementById('form2')
+
+form_A6.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A7 = document.getElementById('text_A7').value
+
+	if (inputText_A7 === '' || inputText_A7 === null) return
+	if (inputText_A7 === correctWord_A7) {
+		itemsToBeDisplayed_A7.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Across 8
+const correctWord_A8 = 'djoser'
+const itemsToBeDisplayed_A8 = document.querySelectorAll('.id_A8')
+const form_A8 = document.getElementById('form2')
+
+form_A8.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_A8 = document.getElementById('text_A8').value
+
+	if (inputText_A8 === '' || inputText_A8 === null) return
+	if (inputText_A8 === correctWord_A8) {
+		itemsToBeDisplayed_A8.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+
+
+
+//Down //
+//Down 1
+const correctWord_D1 = 'mummification'
+const itemsToBeDisplayed_D1 = document.querySelectorAll('.id_D1')
+const form_D1 = document.getElementById('form3')
+
+form_D1.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D1 = document.getElementById('text_D1').value
+
+	if (inputText_D1 === '' || inputText_D1 === null) return
+	if (inputText_D1 === correctWord_D1) {
+		itemsToBeDisplayed_D1.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Down 2
+const correctWord_D2 = 'hieroglyphs'
+const itemsToBeDisplayed_D2 = document.querySelectorAll('.id_D2')
+const form_D2 = document.getElementById('form3')
+
+form_D2.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D2 = document.getElementById('text_D2').value
+
+	if (inputText_D2 === '' || inputText_D2 === null) return
+	if (inputText_D2 === correctWord_D2) {
+		itemsToBeDisplayed_D2.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Down 3
+const correctWord_D3 = 'kabechet'
+const itemsToBeDisplayed_D3 = document.querySelectorAll('.id_D3')
+const form_D3 = document.getElementById('form3')
+
+form_D3.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D3 = document.getElementById('text_D3').value
+
+	if (inputText_D3 === '' || inputText_D3 === null) return
+	if (inputText_D3 === correctWord_D3) {
+		itemsToBeDisplayed_D3.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Down 4
+const correctWord_D4 = 'set'
+const itemsToBeDisplayed_D4 = document.querySelectorAll('.id_D4')
+const form_D4 = document.getElementById('form3')
+
+form_D4.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D4 = document.getElementById('text_D4').value
+
+	if (inputText_D4 === '' || inputText_D4 === null) return
+	if (inputText_D4 === correctWord_D1) {
+		itemsToBeDisplayed_D1.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Down 5
+const correctWord_D5 = 'ka'
+const itemsToBeDisplayed_D5 = document.querySelectorAll('.id_D5')
+const form_D5 = document.getElementById('form3')
+
+form_D5.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D5 = document.getElementById('text_D5').value
+
+	if (inputText_D5 === '' || inputText_D5 === null) return
+	if (inputText_D5 === correctWord_D5) {
+		itemsToBeDisplayed_D5.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Down 6
+const correctWord_D6 = 'bannu'
+const itemsToBeDisplayed_D6 = document.querySelectorAll('.id_D6')
+const form_D6 = document.getElementById('form3')
+
+form_D6.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D6 = document.getElementById('text_D6').value
+
+	if (inputText_D6 === '' || inputText_D6 === null) return
+	if (inputText_D6 === correctWord_D6) {
+		itemsToBeDisplayed_D6.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Down 7
+const correctWord_D7 = 'osiris'
+const itemsToBeDisplayed_D7 = document.querySelectorAll('.id_D7')
+const form_D7 = document.getElementById('form3')
+
+form_D7.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D7 = document.getElementById('text_D7').value
+
+	if (inputText_D7 === '' || inputText_D7 === null) return
+	if (inputText_D7 === correctWord_D7) {
+		itemsToBeDisplayed_D7.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
+//Down 8
+const correctWord_D8 = 'fayous'
+const itemsToBeDisplayed_D8 = document.querySelectorAll('.id_D8')
+const form_D8 = document.getElementById('form3')
+
+form_D8.addEventListener('submit', e => {
+	e.preventDefault()
+	let inputText_D8 = document.getElementById('text_D8').value
+
+	if (inputText_D8 === '' || inputText_D8 === null) return
+	if (inputText_D8 === correctWord_D8) {
+		itemsToBeDisplayed_D8.forEach(item => {
+			item.style.color = 'rgb(255, 255, 255)'
+		})
+	}
+})
